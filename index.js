04ab3d490730cdc2eef58e9325ab9c1e8919bb97
Ronald kubasu <ronald.kubasu@student.moringaschool.com>
2022-08-19 14:44:38 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2)
+}
+const returnLastTwoDrivers=function(array){
+    return array.slice(-2,)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function(factor){
+    return function(fare){
+        return(fare*factor)
+    }
+}
+const fareDoubler = function(fareOne){
+    return(createFareMultiplier(fareOne)(2))
+    
+}
+const fareTripler = function(fareOne){
+    return(createFareMultiplier(fareOne)(3))
+}
+const selectDifferentDrivers = function( array, func){
+    return func(array)
+}
